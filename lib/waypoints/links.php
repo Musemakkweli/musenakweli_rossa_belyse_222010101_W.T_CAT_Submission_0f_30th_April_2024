c5fc35e1dd53bcebe527c0ce73aa4816bac24e05
Musemakkweli <160464054+Musemakkweli@users.noreply.github.com>
2024-04-30 14:21:25 +0200

    Add files via upload

---
+++ lib/waypoints/links.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+*Created by: MUSEMAKWELI Belyse Rossa Arlande 
+* Registration Number: 222010101 
+*School Department: Year 2 in BIT (Business Information Technology)
+*Project: Online Pregnancy Test System
+ *we used the Bootstrap frameworks in development
+ */
+
+  $links = array(
+    'js' => 'lib/waypoints/waypoints.min.js'
+  );
+?>
+
+
